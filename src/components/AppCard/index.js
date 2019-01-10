3464f2f8237b5e27d74ad67bf3fffe2a93fd18b5
Taimoor Khan <taimoor.m.k@live.com>
2019-01-10 20:39:15 +0500

    added styledComponents

---
+++ src/components/AppCard/index.js
@@ -0,0 +1,12 @@
+/*
+    Author: Taimoor Khan
+    GitHub: https://github.com/Taimoormk
+    Email: taimoor.m.k AT LIVE.COM 
+ */
+
+import styled from "styled-components";
+import { Card } from "react-native-paper";
+
+const AppCard = styled(Card)``;
+
+export default AppCard;
