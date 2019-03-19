078460d9e70c6272611cfd3c1eb44da3fa9447d4
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-03-19 10:06:21 +0500

    added Dashboard test case

---
+++ src/__test__/Dashboard.test.js
@@ -0,0 +1,32 @@
+/*
+  Author: Taimoor Khan
+  GitHub: https://github.com/Taimoormk
+  Email: taimoor.m.k AT LIVE.COM 
+*/
+
+// ########## Import Dependencies Here ##########
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import configureStore from "redux-mock-store";
+
+// ########## Import Components Here ##########
+import Dashboard from "../containers/Dashboard";
+
+// ########## Create Initial State ##########
+const fakeInitialState = {
+  dashboardReducer: {
+    data: null,
+    error: null,
+  }
+};
+
+const mockStore = configureStore();
+
+test("renders Dashboard componenent correctly", () => {
+  const renderer = new ShallowRenderer();
+  const store = mockStore(fakeInitialState);
+  const dashboardAttempt = () => null;
+  renderer.render(<Dashboard store={store} dashboardAttempt={dashboardAttempt} />);
+  const wrapper = renderer.getRenderOutput();
+  expect(wrapper).toMatchSnapshot();
+});
