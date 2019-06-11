adb75c3aa79958f7c73d327ca674cb9709df5f85
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-06-11 15:54:55 +0500

    test case passing

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /*
   Author: Taimoor Khan
   GitHub: https://github.com/Taimoormk
@@ -5,33 +6,43 @@
 */
 
 // ########## Import Dependencies Here ##########
-import React from "react";
-import ShallowRenderer from "react-test-renderer/shallow";
-import configureStore from "redux-mock-store";
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import configureStore from 'redux-mock-store';
 
 // ########## Import Components Here ##########
-import Dashboard from "../containers/Dashboard";
+import Dashboard from '../containers/Dashboard';
 
 // ########## Create Initial State ##########
 const fakeInitialState = {
   dashboardReducer: {
     data: null,
     error: null,
-  }
+  },
 };
 
 const mockStore = configureStore();
 
-test("renders Dashboard componenent correctly", () => {
+test('renders Dashboard componenent correctly', () => {
   const renderer = new ShallowRenderer();
-  const name = "testName";
-  const bio = "textBio";
-  const company = "testCompany";
-  const location = "testLocation";
-  const avatarUrl = "testAvatarUrl";
+  const name = 'testName';
+  const bio = 'testBio';
+  const company = 'testCompany';
+  const location = 'testLocation';
+  const avatarUrl = 'testAvatarUrl';
   const store = mockStore(fakeInitialState);
   const dashboardAttempt = () => null;
-  renderer.render(<Dashboard store={store} name={name} bio={bio} company={company} location={location} avatarUrl={avatarUrl} dashboardAttempt={dashboardAttempt} />);
+  renderer.render(
+    <Dashboard
+      store={store}
+      name={name}
+      bio={bio}
+      company={company}
+      location={location}
+      avatarUrl={avatarUrl}
+      dashboardAttempt={dashboardAttempt}
+    />
+  );
   const wrapper = renderer.getRenderOutput();
   expect(wrapper).toMatchSnapshot();
 });
