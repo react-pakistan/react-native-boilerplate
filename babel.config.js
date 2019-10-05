950f84d140d180d29ec2b548215dbc23e3128719
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-10-05 22:24:10 +0500

    :recycle: fixed test

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    test: {
+      presets: [
+        'module:metro-react-native-babel-preset',
+      ],
+    },
+  },
+  presets: [
+    'module:metro-react-native-babel-preset',
+  ],
 };
