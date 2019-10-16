08c17a5d9afb276706dafef738f1e2cc843094be
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-10-16 10:00:07 +0500

    :sparkles: added tests and snaps

---
+++ src/__tests__/MainScreen.test.js
@@ -0,0 +1,44 @@
+// ########## Import Dependencies Here ##########
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import configureStore from 'redux-mock-store';
+
+// ########## Import Components Here ##########
+import MainScreen from '../containers/MainScreen';
+
+jest.mock('react-native-gesture-handler', () => ({
+  BaseButton: () => {},
+}));
+jest.mock('react-navigation', () => ({
+  createAppContainer: jest.fn().mockReturnValue(() => ({})),
+  createDrawerNavigator: jest.fn().mockImplementation(() => ({})),
+  createStackNavigator: jest.fn().mockImplementation(() => ({})),
+  ThemeColors: {
+    light: {
+      bodyContent: '',
+    },
+    dark: {
+      bodyContent: '',
+    },
+  },
+}));
+// jest.mock('react-native-reanimated', () => {});
+
+// ########## Create Initial State ##########
+const fakeInitialState = {
+  showAppReducer: {
+    showApp: true,
+  },
+};
+
+const mockStore = configureStore();
+
+test('renders MainScreen compomenent correctly', () => {
+  const renderer = new ShallowRenderer();
+  const showApp = false;
+  const store = mockStore(fakeInitialState);
+  const showAppAction = () => null;
+  renderer.render(<MainScreen store={store} showApp={showApp} showAppAction={showAppAction} />);
+  const wrapper = renderer.getRenderOutput();
+  expect(wrapper).toMatchSnapshot();
+});
---
+++ src/__tests__/Profile.test.js
@@ -0,0 +1,13 @@
+// ########## Import Dependencies Here ##########
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+// ########## Import Components Here ##########
+import Profile from '../containers/Profile';
+
+test('renders Profile compomenent correctly', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Profile />);
+  const wrapper = renderer.getRenderOutput();
+  expect(wrapper).toMatchSnapshot();
+});
