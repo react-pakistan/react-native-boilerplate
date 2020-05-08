47b13597c7ce1f76e54ce21ef2ab4adabaffc2eb
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-05-08 20:23:05 +0500

    :recycle: minor adjustments

--- src/globals/api-caller.js
+++ src/globals/api-caller.js
@@ -1,24 +1,17 @@
 // ########## Import Dependencies Here ##########
 import axios from 'axios';
-import caseConverter from 'change-case-object';
 
 // ########## Import Components Here ##########
 import { BASE_URL as baseURL, REQUEST_TYPES } from './endpoints';
 
-export default (method = REQUEST_TYPES.GET, url, params = null, data = {}, token = '') => axios({
+export default (method = REQUEST_TYPES.GET, url) => axios({
   method,
   baseURL,
   url,
-  params,
-  data,
-  transformRequest: [(reqData) => JSON.stringify(caseConverter.snakeCase(reqData))],
-  transformResponse: [(respData) => caseConverter.camelCase(respData)],
   headers: {
-    Authorization: token || '',
     'Content-Type': 'application/json',
   },
   responseType: 'json',
-  validateStatus: (status) => status >= 200 && status < 300,
 })
   .then(({ data: resp }) => resp)
   .catch((error) => error);
--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,16 +1,18 @@
+import { ActivityIndicator } from '@taimoormk/react-native-commons-collection/activity-indicator';
 import { Text } from '@taimoormk/react-native-commons-collection/text';
 import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ProfileWrapper, ProfileHeading } from './styled';
 import { fetchProfileData } from './actions';
 import { profileScreenText } from './helpers';
-import { getUserDataFromState } from './selectors';
+import { getUserDataFromState, getUserIsLoadingFromState } from './selectors';
 
 export const Profile = () => {
   // dispatch
   const dispatch = useDispatch();
 
   // selector
+  const isLoading = useSelector(getUserIsLoadingFromState);
   const userData = useSelector(getUserDataFromState);
 
   const fetchData = () => (
@@ -26,6 +28,7 @@ export const Profile = () => {
       <ProfileHeading>
         {profileScreenText.mainHeading}
       </ProfileHeading>
+      {isLoading && <ActivityIndicator />}
       {userData && (
         <Fragment>
           <Text>{userData.name}</Text>
