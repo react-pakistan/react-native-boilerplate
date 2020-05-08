d44bf64de95c7fef1796f720f523047558a6d30c
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-05-08 22:23:23 +0500

    :recycle: minor fixes

--- src/screens/main-screen/constants/index.js
+++ src/screens/main-screen/constants/index.js
@@ -1,4 +1,4 @@
-// ########## Import MainScreen Actions Here ##########
+// ########## Import Components Here ##########
 export const SHOW_APP = 'SHOW_APP';
 export const HIDE_APP = 'HIDE_APP';
 export const HIDE_INTRO = 'HIDE_INTRO';
--- src/screens/main-screen/main-screen.js
+++ src/screens/main-screen/main-screen.js
@@ -1,11 +1,14 @@
+// ########## Import Dependencies Here ##########
 import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
+
+// ########## Import Components Here ##########
+import { AppRouter } from '../../globals/router';
+import { SignIn } from '../sign-in';
 import { showAppAction } from './actions';
 import {
   getShowApp,
 } from './selectors';
-import { SignIn } from '../sign-in';
-import { AppRouter } from '../../globals/router';
 
 export const MainScreen = () => {
   // selector
