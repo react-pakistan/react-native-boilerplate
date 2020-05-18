aa66dfeadf64cb2c81099c7b5521177164575481
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-05-18 13:16:07 +0500

    :recycle: minor amends

--- src/components/drawer-content.js
+++ src/components/drawer-content.js
@@ -10,29 +10,30 @@ import { DrawerContentWrapper, DrawerContentHeader, DrawerBanner } from './style
 export const DrawerContent = ({
   navigation,
   descriptors,
-}) => (
+}) => {
+  console.log('des', descriptors); return (
   <DrawerContentWrapper>
     <DrawerContentHeader>
       <DrawerBanner
         loadingIndicatorSource
-        source={{ uri: 'https://res.cloudinary.com/dq6hflqwx/image/upload/v1565548545/GitHub/React_Pakistan_Storybook_Logo.jpg' }}
         resizeMode='contain'
+        source={{ uri: 'https://res.cloudinary.com/dq6hflqwx/image/upload/v1565548545/GitHub/React_Pakistan_Storybook_Logo.jpg' }}
       />
     </DrawerContentHeader>
     <FlatList
       data={Object.keys(descriptors)}
       renderItem={({ item, index }) => (
         <Text
-          key={item}
           onPress={() => navigation.navigate(Object.keys(descriptors)[index].split('-')[0])}
         >
           {(item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()).split('-')[0]}
         </Text>
       )}
-      keyExtractor={({ id }) => id}
+      keyExtractor={(item) => item}
     />
   </DrawerContentWrapper>
-);
+  );
+};
 
 DrawerContent.propTypes = {
   navigation: shape({
