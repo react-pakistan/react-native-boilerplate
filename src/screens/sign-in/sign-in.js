ea5a4c009e897e3dadfa500fae01fdfa9c5180d3
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-12-02 23:42:49 +0500

    :recycle: refactoring redux layer

--- src/screens/sign-in/sign-in.js
+++ src/screens/sign-in/sign-in.js
@@ -1,10 +1,12 @@
 // ########## Import Dependencies Here ##########
 import { Button } from '@react-pakistan/react-native-commons-collection';
-import { func } from 'prop-types';
+import { Spacer } from '@react-pakistan/react-native-commons-collection/spacer';
+import { theme as defaultTheme } from '@react-pakistan/util-react-native-functions';
+import { object } from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { withTheme } from 'styled-components';
 import {
-  Spacer,
   OAuthHeading,
   SignInWrapper,
 } from './styled';
@@ -13,7 +15,9 @@ import {
 import { loginScreenText } from './helpers';
 import { showAppAction } from '../../redux/actions';
 
-export const SignIn = () => {
+export const SignIn = withTheme(({
+  theme,
+}) => {
   // dispatch
   const dispatch = useDispatch();
 
@@ -22,20 +26,23 @@ export const SignIn = () => {
       <OAuthHeading>
         {loginScreenText.oAuthHeading}
       </OAuthHeading>
-      <Spacer margin={2} />
+      <Spacer margin={theme.spacing.default} />
       <Button
         onPress={() => dispatch(showAppAction())}
         title={loginScreenText.facebookButton}
       />
-      <Spacer margin={2} />
+      <Spacer margin={theme.spacing.default} />
       <Button
         onPress={() => dispatch(showAppAction())}
         title={loginScreenText.googleButton}
       />
     </SignInWrapper>
   );
-};
+});
 
 SignIn.propTypes = {
-  showAppAction: func,
+  theme: object,
+};
+SignIn.defaultProps = {
+  theme: defaultTheme,
 };
