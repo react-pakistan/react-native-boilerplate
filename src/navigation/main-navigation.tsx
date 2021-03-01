b0e52bf1af0af93b6b942bc7c95b32bc45b822d4
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2021-03-01 21:35:34 +0500

    :recyle: app restructing

---
+++ src/navigation/main-navigation.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment, ReactElement } from 'react';
+import { useSelector } from 'react-redux';
+import { getHideIntro } from '../redux/selectors';
+import { MainScreen } from '../screens/main-screen';
+import { OnBoarding } from '../screens/on-boarding';
+
+export const MainNavigation = () : ReactElement => {
+  const hideIntro = useSelector(getHideIntro);
+
+  return (
+    <Fragment>
+      {hideIntro ? <OnBoarding /> : <MainScreen />}
+    </Fragment>
+  );
+};
