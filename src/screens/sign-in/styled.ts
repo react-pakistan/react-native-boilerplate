fa64523f46d169451d6798c4efeb7a7f327f47da
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2021-03-11 21:40:21 +0500

    :recycle: minod amends

--- src/screens/sign-in/styled.ts
+++ src/screens/sign-in/styled.ts
@@ -1,16 +1,18 @@
+import { IRNTheme } from '@react-pakistan/util-react-native-functions';
 import { Text, View } from '@react-pakistan/react-native-commons-collection';
+import { ThemedStyledProps } from 'styled-components';
 import styled from 'styled-components/native';
 
 export const SignInWrapper = styled(View)`
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.lightGrey};
+  background-color: ${({ theme } : ThemedStyledProps<{}, IRNTheme>) : string => theme.colors.lightGrey};
   flex: 1;
   justify-content: center;
 `;
 
 export const OAuthHeading = styled(Text)`
-  color: ${({ theme }) => theme.colors.black};
-  font-family: ${({ theme }) => theme.typography.fontFamily};
-  font-size: ${({ theme }) => theme.spacing.default}px;
+  color: ${({ theme } : ThemedStyledProps<{}, IRNTheme>) : string => theme.colors.black};
+  font-family: ${({ theme } : ThemedStyledProps<{}, IRNTheme>) : string => theme.typography.fontFamily};
+  font-size: ${({ theme } : ThemedStyledProps<{}, IRNTheme>) : number => theme.spacing.default}px;
   text-transform: uppercase;
 `;
