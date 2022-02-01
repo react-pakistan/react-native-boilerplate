432465c6e09982a247479e5c559b03beb210e1d2
Taimoor Khan <taimoor.m.k@live.com>
2022-02-01 19:54:20 +0500

    :recycle: rebased

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,13 +1,16 @@
-// ########## Import Dependencies Here ##########
+import { ThemeProvider } from 'styled-components/native';
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
-
-// ########## Import Components Here ##########
 import { Dashboard } from '../screens/dashboard';
+import { appTheme } from '../theme';
 
 test('renders Dashboard component correctly', () => {
   const renderer = new ShallowRenderer();
-  renderer.render(<Dashboard />);
+  renderer.render(
+    <ThemeProvider theme={appTheme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
   const wrapper = renderer.getRenderOutput();
   expect(wrapper).toMatchSnapshot();
 });
